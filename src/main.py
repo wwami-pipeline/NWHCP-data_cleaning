f3f93339bd7058cd98936fd3ee63d26b500ed692
Loi Cheng <loi@preverna.com>
2021-11-06 14:26:30 -0400

    new data import

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
-from utils import import_from_redcap
-import logging
-import schedule
-import time
-import os
-import requests
-import json
 import datetime
+import json
+import logging
+import time
+
 from pymongo import MongoClient
 from pymongo.write_concern import WriteConcern
 
+from utils import import_from_redcap
 
-# read redcap data and import to mongodb
+OUT_PUT_JSON_PATH = "src/output/pipelinesurveydata.json"
+
+
 def import_data():
+    """
+    DEPRECATED
+    read redcap data and import to mongodb
+    """
+
     data = import_from_redcap.get_cleaned_data()
 
     # write data to mongo-like json file
@@ -33,11 +38,33 @@ def import_data():
     collection.with_options(write_concern=WriteConcern(w=0)).insert_many(file_data)
 
 
+def load_data():
+    """
+    read redcap data and import to mongodb
+    """
+
+    import_from_redcap.get_json()
+
+    # connect to mongo
+    myclient = MongoClient("mongodb://localhost:27017/")
+
+    # database
+    db = myclient["mongodb"]
+    collection = db["organization"]
+
+    # Loading or Opening the json file
+    with open(OUT_PUT_JSON_PATH) as file:
+        file_data = json.load(file)
+
+    # ignore duplicated data
+    collection.with_options(write_concern=WriteConcern(w=0)).insert_many(file_data)
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     while True:
         # import data
-        import_data()
+        load_data()
         print(str(datetime.datetime.now()) + " imported data, wait 1hr", flush=True)
         # repeat
         time.sleep(3600)
